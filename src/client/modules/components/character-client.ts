ea07f9a096b33281d2ecb009a5c0fdd490524c4a
noahmagcalas <noah.magcalas@gmail.com>
2024-05-30 17:11:09 -0500

    integrated health bar
    also enabled streaming and made startercharacter an atomic model

--- src/client/modules/components/character-client.ts
+++ src/client/modules/components/character-client.ts
@@ -6,6 +6,7 @@ import {
 } from "shared/modules/components/character";
 import { Events } from "../networking";
 import { Workspace } from "@rbxts/services";
+import { HudController } from "../controllers/hud-controller";
 
 const events = Events.characterEvents;
 
@@ -16,14 +17,22 @@ export class CharacterClient extends Character<
 	CharacterAttributes,
 	CharacterInstance
 > {
+	constructor(private hudController: HudController) {
+		super();
+	}
+
 	override onStart(): void {
 		super.onStart();
 
 		this.trove.add(events.killed.connect(() => this.onKilled()));
 	}
 
-	protected onHealthChanged(health: number): void {
-		// change health bar
+	override onHealthChanged(health: number): void {
+		print("health", health);
+		this.hudController.updateHealth(
+			health,
+			this.instance.Humanoid.MaxHealth,
+		);
 	}
 
 	onKilled(): void {
