f3e30ff9250f7db69f0cfb970cdad1b4ba72e64f
noahmagcalas <noah.magcalas@gmail.com>
2024-06-16 23:02:32 -0500

    feat: install and configure cmdr

--- src/scripts/player/main.client.ts
+++ src/scripts/player/main.client.ts
@@ -1,11 +1,14 @@
 import { Flamework, Modding } from "@flamework/core";
+import { CmdrClient as Cmdr } from "@rbxts/cmdr";
 import Log, { Logger } from "@rbxts/log";
 
 Log.SetLogger(Logger.configure().WriteTo(Log.RobloxOutput()).Create());
 
+Cmdr.SetActivationKeys([Enum.KeyCode.RightBracket]);
+
 Flamework.addPaths("src/shared//components");
-Flamework.addPaths("src/server//services");
-Flamework.addPaths("src/server//components");
+Flamework.addPaths("src/client//components");
+Flamework.addPaths("src/client//controllers");
 
 Modding.registerDependency<Logger>((ctor) => {
 	return Log.ForContext(ctor);
