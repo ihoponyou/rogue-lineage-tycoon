79b816f1ab4aef48cd5196fee9953a2eef0b92d6
noahmagcalas <noah.magcalas@gmail.com>
2024-06-21 15:00:15 -0500

    refactor: move toggleable implementation into base interactable

--- src/server/components/interactable/index.ts
+++ src/server/components/interactable/index.ts
@@ -1,14 +1,19 @@
 import { Component } from "@flamework/components";
 import Signal from "@rbxts/signal";
 import { DisposableComponent } from "shared/components/disposable-component";
+import { Toggleable } from "shared/toggleable";
 
 type InteractedCallback = (player: Player) => void;
 
 @Component()
 export abstract class Interactable<
-	A extends {} = {},
-	I extends Instance = Instance,
-> extends DisposableComponent<A, I> {
+		A extends {} = {},
+		I extends Instance = Instance,
+	>
+	extends DisposableComponent<A, I>
+	implements Toggleable
+{
+	private _isEnabled = false;
 	private interacted = new Signal<InteractedCallback>();
 
 	public interact(player: Player): void {
@@ -17,5 +22,17 @@ export abstract class Interactable<
 
 	public onInteracted(callback: InteractedCallback): void {
 		this.trove.connect(this.interacted, (player) => callback(player));
+	}
+
+	public isEnabled(): boolean {
+		return this._isEnabled;
+	}
+
+	public enable(): void {
+		this._isEnabled = true;
+	}
+
+	public disable(): void {
+		this._isEnabled = false;
 	}
 }
