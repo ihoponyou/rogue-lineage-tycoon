f3a99f6ebb7222371a2620e3f96e1bbaaeda9331
noahmagcalas <noah.magcalas@gmail.com>
2024-06-21 14:19:56 -0500

    fix: add proper types for custom inject decorator

--- src/shared/inject.ts
+++ src/shared/inject.ts
@@ -1,21 +1,20 @@
+// https://github.com/Tesmi-Develop
+// https://discord.com/channels/476080952636997633/1250858120679260212/1250859525683019796
 import { Dependency, Modding, Reflect } from "@flamework/core";
-import { Constructor } from "@flamework/core/out/utility";
+import { Constructor, AbstractConstructor } from "@flamework/core/out/utility";
 
-// the types here got destroyed
-export const ModifyConstructorMethod = <
-	T extends Constructor,
-	C extends Callback = Callback,
->(
-	_constructor: T,
-	methodName: "constructor",
-	visitor: (
-		originalMethod: (...args: Array<unknown>) => void,
-	) => (this: object, ...args: Array<unknown>) => unknown,
-): T => {
-	const modifiedMethod = visitor(_constructor[methodName as never]);
-	_constructor[methodName as never] = modifiedMethod as never;
-	return _constructor;
-};
+type ReturnMethods<T extends object> = ExtractKeys<T, Callback>;
+type TMethod<T> = (
+	self: InferThis<T>,
+	...parameters: Parameters<T>
+) => ReturnType<T>;
+
+type GetContextFromConstructors<T> =
+	T extends Constructor<infer C>
+		? C
+		: T extends AbstractConstructor<infer C>
+			? C
+			: never;
 
 const INJECT_KEY = "Inject";
 
@@ -55,4 +54,22 @@ export const Inject = (ctor: object, property: string) => {
 				return originalConstructor(this, ...args);
 			},
 	);
+};
+
+export const ModifyConstructorMethod = <
+	T extends Constructor | AbstractConstructor,
+	C extends Callback = Callback,
+>(
+	_constructor: T,
+	methodName: ReturnMethods<GetContextFromConstructors<T>> | "constructor",
+	visitor: (
+		originalMethod: TMethod<C>,
+	) => (
+		this: GetContextFromConstructors<T>,
+		...args: Array<unknown>
+	) => unknown,
+): T => {
+	const modifiedMethod = visitor(_constructor[methodName as never]);
+	_constructor[methodName as never] = modifiedMethod as never;
+	return _constructor;
 };
