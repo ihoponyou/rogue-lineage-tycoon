d8062a07105478e9a352b19f9c89f1529e2f1833
noahmagcalas <noah.magcalas@gmail.com>
2024-07-21 14:48:30 -0500

    refactor: improve touchable component architecture

--- src/server/components/interactable/touchable/index.ts
+++ src/server/components/interactable/touchable/index.ts
@@ -1,21 +1,15 @@
 import { Component } from "@flamework/components";
-import { OnStart } from "@flamework/core";
-import { getCharacterFromBodyPart } from "shared/getCharacterFromBodyPart";
 import { Players } from "@rbxts/services";
+import { getCharacterFromBodyPart } from "shared/getCharacterFromBodyPart";
 import { Interactable } from "..";
 
 type TouchableInstance = BasePart | Model;
 
 @Component()
 export abstract class Touchable<
-		A extends {} = {},
-		I extends TouchableInstance = TouchableInstance,
-	>
-	extends Interactable<A, I>
-	implements OnStart
-{
-	public abstract onStart(): void;
-
+	A extends {} = {},
+	I extends TouchableInstance = TouchableInstance,
+> extends Interactable<A, I> {
 	protected tryInteraction(part: BasePart): void {
 		if (!this.isEnabled()) return;
 		const character = getCharacterFromBodyPart(part);
--- src/server/components/interactable/touchable/touchable-part.ts
+++ src/server/components/interactable/touchable/touchable-part.ts
@@ -1,13 +1,14 @@
 import { Component } from "@flamework/components";
+import { OnStart } from "@flamework/core";
 import { Touchable } from ".";
 
 @Component({
 	tag: "TouchablePart",
 })
-export class TouchablePart<
-	A extends {} = {},
-	I extends BasePart = BasePart,
-> extends Touchable<A, I> {
+export class TouchablePart<A extends {} = {}, I extends BasePart = BasePart>
+	extends Touchable<A, I>
+	implements OnStart
+{
 	public onStart(): void {
 		this.trove.connect(this.instance.Touched, (otherPart) => {
 			this.tryInteraction(otherPart);
