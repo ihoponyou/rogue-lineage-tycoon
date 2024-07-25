d42f1052a2e608e7bfab81945060018d160d101c
noahmagcalas <noah.magcalas@gmail.com>
2024-07-25 14:04:46 -0500

    feat: add new assets

--- src/server/configs/tycoon.ts
+++ src/server/configs/tycoon.ts
@@ -2,24 +2,24 @@ import { ReplicatedStorage } from "@rbxts/services";
 import { Currency } from "../../../types/currency";
 
 export interface AssetConfig {
-	cost: number;
-	currency: Currency;
-	prerequisites: Array<string>;
+	readonly cost: number;
+	readonly currency: Currency;
+	readonly prerequisites: Array<string>;
 }
 
 interface DropperConfig {
-	dropsPerSecond: number;
-	productsPerDrop: number;
-	productModel: Model;
+	readonly dropsPerSecond: number;
+	readonly productsPerDrop: number;
+	readonly productModels: Array<Model>;
 }
 
 interface ProductConfig {
-	currency: Currency;
-	value: number;
+	readonly currency: Currency;
+	readonly value: number;
 }
 
 interface UpgraderConfig {
-	multiplier: number;
+	readonly multiplier: number;
 }
 
 const PRODUCT_MODELS = ReplicatedStorage.Assets.Tycoon.Products;
@@ -55,6 +55,21 @@ export const ASSETS: { [name: string]: AssetConfig } = {
 		cost: 10,
 		currency: "Silver",
 		prerequisites: ["Walls", "ManaRocks"],
+	},
+	ManaRun: {
+		cost: 150,
+		currency: "Silver",
+		prerequisites: ["Mana"],
+	},
+	ManaDash: {
+		cost: 250,
+		currency: "Silver",
+		prerequisites: ["ManaRun"],
+	},
+	ManaClimb: {
+		cost: 500,
+		currency: "Silver",
+		prerequisites: ["ManaDash"],
 	},
 	DoorRocks: {
 		cost: 0,
@@ -93,13 +108,92 @@ export const PRODUCTS: { [name: string]: ProductConfig } = {
 		currency: "Silver",
 		value: 1,
 	},
+	"???": {
+		currency: "Silver",
+		value: 50,
+	},
+	Diamond: {
+		currency: "Silver",
+		value: 35,
+	},
+	Ruby: {
+		currency: "Silver",
+		value: 14,
+	},
+	Emerald: {
+		currency: "Silver",
+		value: 14,
+	},
+	Sapphire: {
+		currency: "Silver",
+		value: 14,
+	},
+	Opal: {
+		currency: "Silver",
+		value: 9,
+	},
+	"Idol of the Forgotten": {
+		currency: "Silver",
+		value: 8,
+	},
+	Amulet: {
+		currency: "Silver",
+		value: 7,
+	},
+	Ring: {
+		currency: "Silver",
+		value: 7,
+	},
+	"Old Amulet": {
+		currency: "Silver",
+		value: 6,
+	},
+	"Old Ring": {
+		currency: "Silver",
+		value: 6,
+	},
+	Goblet: {
+		currency: "Silver",
+		value: 5,
+	},
+	Scroll: {
+		currency: "Silver",
+		value: 15,
+	},
+	Godscroll: {
+		currency: "Silver",
+		value: 50,
+	},
 };
 
 export const DROPPERS: { [name: string]: DropperConfig } = {
 	SilverDropper: {
-		dropsPerSecond: 5,
+		dropsPerSecond: 3,
 		productsPerDrop: 1,
-		productModel: PRODUCT_MODELS.shilling,
+		productModels: [PRODUCT_MODELS.shilling],
+	},
+	TrinketDropper: {
+		dropsPerSecond: 1,
+		productsPerDrop: 1,
+		productModels: [
+			PRODUCT_MODELS["???"],
+			PRODUCT_MODELS.Diamond,
+			PRODUCT_MODELS.Ruby,
+			PRODUCT_MODELS.Emerald,
+			PRODUCT_MODELS.Sapphire,
+			PRODUCT_MODELS.Opal,
+			PRODUCT_MODELS["Idol of the Forgotten"],
+			PRODUCT_MODELS.Amulet,
+			PRODUCT_MODELS.Ring,
+			PRODUCT_MODELS["Old Amulet"],
+			PRODUCT_MODELS["Old Ring"],
+			PRODUCT_MODELS.Goblet,
+		],
+	},
+	ScrollDropper: {
+		dropsPerSecond: 0.5,
+		productsPerDrop: 1,
+		productModels: [PRODUCT_MODELS.Scroll, PRODUCT_MODELS.Godscroll],
 	},
 };
 
