931610bb7c394798de8ecfd0bb768d1c411eff86
noahmagcalas <noah.magcalas@gmail.com>
2024-08-05 10:00:45 -0500

    refactor: use shared selectors in serverside selectors

--- src/server/store/selectors.ts
+++ src/server/store/selectors.ts
@@ -1,5 +1,12 @@
 import { createSelector } from "@rbxts/reflex";
+import { selectConditions } from "shared/store/slices/conditions/selectors";
+import { selectCurrencies } from "shared/store/slices/currencies/selectors";
+import { selectIdentity } from "shared/store/slices/identity/selectors";
+import { selectMana } from "shared/store/slices/mana/selectors";
 import { PlayerData } from "shared/store/slices/player-data";
+import { selectResources } from "shared/store/slices/resources/selectors";
+import { selectStats } from "shared/store/slices/stats/selectors";
+import { selectTransform } from "shared/store/slices/transform/selectors";
 import { RootState } from ".";
 
 export function selectPlayer(player: Player) {
@@ -8,37 +15,51 @@ export function selectPlayer(player: Player) {
 
 export function selectPlayerHealth(player: Player) {
 	return createSelector(
-		selectPlayer(player),
-		(state) => state?.resources.health,
+		selectPlayerResources(player),
+		(state) => state?.health,
 	);
 }
 
 export function selectPlayerConditions(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.conditions);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectConditions()(state),
+	);
 }
 
 export function selectPlayerTransform(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.transform);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectTransform()(state),
+	);
 }
 
 export function selectPlayerCurrencies(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.currencies);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectCurrencies()(state),
+	);
 }
 
 export function selectPlayerStats(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.stats);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectStats()(state),
+	);
 }
 
 export function selectPlayerResources(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.resources);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectResources()(state),
+	);
 }
 
 export function selectPlayerMana(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.mana);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectMana()(state),
+	);
 }
 
 export function selectPlayerIdentity(player: Player) {
-	return createSelector(selectPlayer(player), (state) => state?.identity);
+	return createSelector(selectPlayer(player), (state) =>
+		state === undefined ? undefined : selectIdentity()(state),
+	);
 }
 
 export function selectPlayerData(player: Player) {
