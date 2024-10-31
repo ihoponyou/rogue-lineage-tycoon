4d51812db23fa61b02e92162e00242fd44d8eb09
noahmagcalas <noah.magcalas@gmail.com>
2024-10-30 21:28:31 -0500

    add ragdoll command

---
+++ src/server/commands/ragdoll.ts
@@ -0,0 +1,45 @@
+import { Components } from "@flamework/components";
+import { Modding } from "@flamework/core";
+import {
+	CenturionType,
+	Command,
+	CommandContext,
+	Guard,
+	Register,
+} from "@rbxts/centurion";
+import { PlayerServer } from "server/components/player-server";
+import isAdmin from "shared/commands/guards/is-admin";
+
+@Register()
+@Guard(isAdmin)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class RagdollCommand {
+	@Command({
+		name: "ragdoll",
+		description: "toggle a player character's ragdoll",
+		arguments: [
+			{
+				name: "player",
+				description: ":)",
+				type: CenturionType.Player,
+			},
+			{
+				name: "enable",
+				description: "to toggle or not to toggle",
+				type: CenturionType.Boolean,
+			},
+		],
+	})
+	ragdoll(ctx: CommandContext, player: Player, enable: boolean) {
+		const components = Modding.resolveSingleton(Components);
+		try {
+			const playerServer = components
+				.waitForComponent<PlayerServer>(player)
+				.expect();
+			playerServer.getCharacter().toggleRagdoll(enable);
+			ctx.reply(`${enable ? "R" : "Unr"}agdolled ${player.Name}`);
+		} catch (err) {
+			ctx.error(tostring(err));
+		}
+	}
+}
--- src/server/old shit/give_skillServer.ts
+++
@@ -1,7 +0,0 @@
-import { CommandContext } from "@rbxts/cmdr";
-import { store } from "server/store";
-import { SkillId } from "shared/configs/skills";
-
-export = function (context: CommandContext, player: Player, skillId: SkillId) {
-	store.addSkill(player, skillId);
-};
